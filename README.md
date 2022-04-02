ac8392c23ffe8de0dd1afa226bfcb66606191f3c
colton5007 <sandvikcolton@gmail.com>
2022-04-01 21:52:21 -0500

    Generalized cohomology computations to all Grassmannians G(k,n+1), adds support for all products of Schubert cycles, and adds smaller additional functionalities

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Pieri's
 
-This is a tool for computing the cohomology ring structure of the Grassmannian G(2,n+1) using Schubert calculus. Namely, the tool can compute the cohomology groups using Schubert classes which serve as generating cocycles in the cohomology ring. Using Pieri's formula, we can compute the product between Schubert classes.
-Currently, due to the implementation of Schubert cycles and Pieri's formula, multiplication only works when one of the multiplicands is a class of the form `SchubertCalculus(a=a,b=0,n=n)`; although, this will be fixed shortly. 
+This is a tool for computing the cohomology ring structure of the Grassmannian G(k,n+1) using Schubert calculus. Namely, the tool can compute the cohomology groups using Schubert classes which serve as generating cocycles in the cohomology ring. Using Pieri's formula, we can compute the product between Schubert classes.
 
 ## Documentation
 
@@ -9,18 +8,21 @@ For some basic examples of usage:
 
 ```python
 # To define a Schubert cycle, we use the SchubertCycle class
-s1 = SchubertCycle(a=1,b=0,n=3)
+s1 = SchubertCycle(a=[1],n=3)
 # We can then multiply cycles together or add them using * and +
 s1*s1
+s1 + s1
+```
+
+```
 σ_(1,1)+σ_2 # output of product
 s1 + s1
-2σ_1 # output of sum
 ```
 
 We can also find the generators for the cohomology ring
 
 ```python
-compute_cohomology_groups(n=3)
+compute_cohomology_groups(k=2,n=3)
 [[1], [], [σ_1], [], [σ_2, σ_(1,1)], [], [σ_(2,1)], [], [σ_(2,2)]]
 # The output of this function is stored as a list of lists where the i-th list are the generators of the i-th cohomology class
 ```
@@ -28,8 +30,7 @@ compute_cohomology_groups(n=3)
 While this can be read easily from the output above, if we want a prettified version of the cohomology groups with generators, we can do this easily
 
 ```python
-groups = compute_cohomology_groups(n=3)
-print_cohomology_groups(groups)
+print_cohomology_groups(k=2,n=3)
 ```
 
 ```
@@ -44,10 +45,10 @@ H^7 (G(2,4)) = 0
 H^8 (G(2,4)) = ℤσ_(2,2)
 ```
 
-Finally, we can compute the multiplication table (although the accuracy of the computation is poor for n > 3; namely, when the Pieri's formula implementation does not work, and we can not take advantage of Poincaré duality)
+Finally, we can compute the multiplication table
 
 ```python
-groups = compute_cohomology_groups(n=3)
+groups = compute_cohomology_groups(k=2,n=3)
 print_multiplication_table(groups)
 ```
 
@@ -62,41 +63,95 @@ print_multiplication_table(groups)
 |     σ_(2,2)         |     σ_(2,2)         |     0               |     0               |     0               |     0               |     0               |
 ```
 
+There is also an option to output the multiplication table into a LaTeX tabular environment
+```python
+groups = compute_cohomology_groups(k=2,n=3)
+print_multiplication_table(groups, latex=True)
+```
+```
+\begin{tabular}{ | c | c | c | c | c | c | c | }
+\hline
+ $\cdot$ &  $1$ &  $\sigma_{1}$ &  $\sigma_{1,1}$ &  $\sigma_{2}$ &  $\sigma_{2,1}$ &  $\sigma_{2,2}$ \\
+\hline
+ $1$ &  $1$ &  $\sigma_{1}$ &  $\sigma_{1,1}$ &  $\sigma_{2}$ &  $\sigma_{2,1}$ &  $\sigma_{2,2}$ \\
+\hline
+ $\sigma_{1}$ &  $\sigma_{1}$ &  $\sigma_{1,1}+\sigma_{2}$ &  $\sigma_{2,1}$ &  $\sigma_{2,1}$ &  $\sigma_{2,2}$ &  $0$ \\
+\hline
+ $\sigma_{1,1}$ &  $\sigma_{1,1}$ &  $\sigma_{2,1}$ &  $\sigma_{2,2}$ &  $0$ &  $0$ &  $0$ \\
+\hline
+ $\sigma_{2}$ &  $\sigma_{2}$ &  $\sigma_{2,1}$ &  $0$ &  $\sigma_{2,2}$ &  $0$ &  $0$ \\
+\hline
+ $\sigma_{2,1}$ &  $\sigma_{2,1}$ &  $\sigma_{2,2}$ &  $0$ &  $0$ &  $0$ &  $0$ \\
+\hline
+ $\sigma_{2,2}$ &  $\sigma_{2,2}$ &  $0$ &  $0$ &  $0$ &  $0$ &  $0$ \\
+\hline
+\end{tabular}
+```
+![Output of Multiplication Table in LaTeX](./latexoutputtest.png)
+
+
+The number of generators grow quickly for higher `k` and `n`, so we will not include many examples. But it should be easy to extrapolate if needed. For one, fairly useless example,
+
+```python
+s2 = SchubertCycle(a=[2],k=4,n=9)
+s43 = SchubertCycle(a=[4,3],k=4,n=9)
+s2*s43
+```
+```
+σ_(4,3,2)+σ_(4,4,1)+σ_(5,3,1)+σ_(5,4)+σ_(6,3)
+```
+
+One can also subtract cycles, negate cycles, multiply cycles by integer scalars, and as alluded to before exponentiate. All of these follow the conventional pythonic style
+
+```python
+s1 = SchubertCycle(a=[1],k=2,n=3)
+s1 + s1
+s1 - s1
+3*(s1**2)
+```
+```
+2σ_(1)
+0
+3σ_(1,1)+3σ_(2)
+```
+
 ## Applications
 
 Finally, let's use the software to solve a couple enumerative geometry problems.
 
 ### Problem 1
 
-Q: How many lines pass through the 4 distinct lines in projective 3-space, P^3?
+Q: How many lines pass through the 4 distinct lines in projective 3-space, `P^3`?
 
-To solve this, we first translate the problem to Schubert calculus. The set of lines that pass through a given line is the Schubert cycle σ_1. We can then take the intersection pairing on σ_1^4 to find the number of lines passing through any four given lines (in general position).
+To solve this, we first translate the problem to Schubert calculus. The set of lines that pass through a given line is the Schubert cycle `σ_1`. We can then take the intersection pairing on `σ_1^4` to find the number of lines passing through any four given lines (in general position).
 Let's translate this to code.
 
 ```python
-s1 = SchubertCycle(a=1,b=0,n=3)
+s1 = SchubertCycle(a=[1], k=2, n=3)
 s1**4
-# Note that this exponential works because we can use Poincaré duality to compute (s1*s1)*(s1*s1), in general, the following line is equivalent but works in more situations
-((s1*s1)*s1)*s1
+# or
+(s1*s1)*(s1*s1)
+# or
+s1*s1*s1*s1
 ```
 
-In either case, we obtain the output
+In any case, we obtain the output
 
 ```
 2σ_(2,2)
 ```
 
-And the intersection pairing simply yields the scalar 2. Hence, there are 2 lines that intersect 4 arbitrary lines in P^3
+And the intersection pairing simply yields the scalar 2. Hence, there are 2 lines that intersect 4 arbitrary lines in `P^3`
 
 ### Problem 2
-Q: How many lines pass through 6 distinct planes in projective 4-space, P^4?
+Q: How many lines pass through 6 distinct planes in projective 4-space, `P^4`?
 
-Equivalently the problem boils down to computing σ_1^6 in Schubert calculus since σ_1 can be though of as the set of lines passing through a particular plane.
+Equivalently the problem boils down to computing `σ_1^6` in Schubert calculus since `σ_1` can be though of as the set of lines passing through a particular plane.
 Let's translate this to code.
 
 ```python
-s1 = SchubertCycle(a=1,b=0,n=4)
-((((s1*s1)*s1)*s1)*s1)*s1
+s1 = SchubertCycle(a=[1],k=2,n=4)
+s1**6
 ```
 
 Which yields the output
@@ -105,17 +160,17 @@ Which yields the output
 5σ_(3,3)
 ```
 
-The intersection pairing gives the scalar 5. Hence, there are 5 lines that intersect 6 arbitrary planes in P^4.
+The intersection pairing gives the scalar 5. Hence, there are 5 lines that intersect 6 arbitrary planes in `P^4`.
 
 ### Problem 3
-Q: How many lines pass through 3 distinct planes and a specific point in projective 4-space, P^4?
+Q: How many lines pass through 3 distinct planes and a specific point in projective 4-space, `P^4`?
 
-Similar to problem 2, the problem boils down to computing (σ_1^3)*(σ_3). We can translate this to code.
+Similar to problem 2, the problem boils down to computing `(σ_1^3)*(σ_3)`. We can translate this to code.
 
 ```python
-s1 = SchubertCycle(a=1,b=0,n=4)
-s3 = SchubertCycle(a=3,b=0,n=4)
-((s1*s1)*s1)*s3
+s1 = SchubertCycle(a=[1], k=2, n=4)
+s3 = SchubertCycle(a=[3], k=2, n=4)
+s1*s1*s1*s3
 ```
 
 Which yields the output
@@ -124,11 +179,11 @@ Which yields the output
 σ_(3,3)
 ```
 
-Hence, there is a single line that intersects 3 planes and passes through a fixed point in P^4.
+Hence, there is a single line that intersects 3 planes and passes through a fixed point in `P^4`.
 
 ## Future Work
 
-I would like to generalize this implementation for a general Grassmannian G(k,n+1), and of course, fix the multiplication errors listed above.
+
 
 
 ## Citations
--- pieri.py
+++ pieri.py
@@ -1,38 +1,104 @@
+from copy import copy
 
+def copy_matrix(A):
+	rows = len(A)
+	cols = len(A[0])
+	AC = [[A[i][j] for j in range(rows)] for i in range(cols)]
+	return AC
+
+# Computes the determinant (recursively) using minor reduction of a matrix of Schubert cycles
+def gdeterminant(A,total=None):
+	if not total:
+		total = []
+	indices = list(range(len(A)))
+	# Recursive base case
+	if len(A) == 2 and len(A[0]) == 2:
+		a00 = copy(A[0][0])
+		a01 = copy(A[0][1])
+		a10 = copy(A[1][0])
+		a11 = copy(A[1][1])
+		a10.scalar *= -1
+		return [[a00, a11], [a10,a01]]
+	# Computes minors
+	for fc in indices:
+		As = copy_matrix(A)
+		As = As[1:]
+		height = len(As)
+
+		for i in range(height):
+			As[i] = As[i][0:fc] + As[i][fc+1:]
+
+		sign = (-1) ** (fc % 2)
+
+		sub_det = gdeterminant(As)
+		kfc = copy(A[0][fc])
+		kfc.scalar *= sign
+		
+		this_det = [[kfc] + entry for entry in sub_det]
+		total += this_det
+	return total
+
+# Class for handling sums of Schubert cycles in the cohomology ring
 class CSum:
 	def __init__(self):
 		self.summands = []
 		
+	# Adds a cycle to the sum
 	def add_cycle(self, other):
 		existing = False
 		for i in range(len(self.summands)):
 			summand = self.summands[i]
+			# Check if the currect summand in the list iteration is the same as the to be added summand
 			if summand == other:
-				cycle = SchubertCycle(a=summand.a, b=summand.b, n=summand.n)
-				cycle.scalar = summand.scalar + other.scalar
+				cycle = SchubertCycle(a=summand.a, k=summand.k, n=summand.n, scalar=summand.scalar + other.scalar)
+				# Deletes summand if scalars add to 0
 				if cycle.scalar == 0:
 					del self.summands[i]
+					existing = True
+					break
+				# Modifies summand in the list with updated scalar
 				else:
 					self.summands[i] = cycle
 					existing = True
-				break
-		if not existing:
-			self.summands.append(other)
+					break
+		# Adds new cycle if not present in the list of summands
+		if not existing and other.scalar != 0:
+			self.summands.append(copy(other))
 			
+	# Adds a cycle or another sum of cycles to the sum
 	def add(self, other):
 		if type(other) is SchubertCycle:
 			self.add_cycle(other)
 		elif type(other) is CSum:
 			for cycle in other.summands:
 				self.add_cycle(cycle)
+
+	# Scales the cycles in the sum by an integer scalar
+	def scale(self, alpha):
+		cs = copy(self)
+		for i in range(len(cs.summands)):
+			cs.summands[i].scalar *= alpha
+		return cs
 				
+	# Python method overload for adding a+b
 	def __add__(self, other):
 		self.add(other)
 		return self
+
+	# Python method overload for subtracting a-b
+	def __sub__(self, other):
+		o = other.scale(-1)
+		return self+o
+
+	# Python method overload for negation -a
+	def __neg__(self):
+		return self.scale(-1)
 	
+	# Produces nice string representation for sum class
 	def __repr__(self):
 		return str(self)
 		
+	# For stringifying Schubert cycles
 	def __str__(self):
 		if len(self.summands) == 0:
 			return "0"
@@ -40,11 +106,13 @@ class CSum:
 			str_summands = [str(summand) for summand in self.summands]
 			return "+".join(str_summands)
 
+	# Computes the length of the stringified version of the class. To be used in computing padding for prettified output
 	def str_length(self):
 		str_summands = [str(summand) for summand in self.summands]
 		o = "+".join(str_summands)
 		return len(o)
 
+	# Python method overload for multiplication a*b, uses multiplication in cycle class
 	def __mul__(self, other):
 		output = CSum()
 		if type(other) is SchubertCycle:
@@ -56,40 +124,78 @@ class CSum:
 					output.add(summand1*summand2)
 		return output
 
+	# Python method overload for left multiplication by scalars 
+	def __rmul__(self, alpha):
+		return self.scale(alpha)
+
 class SchubertCycle:
-	def __init__(self, a=0,b=0,n=0):
-		if n-1 >= a and a >= b and b>= 0:
-			self.n = n
-			self.a = a
-			self.b = b
-			self.scalar = 1
-		else:
-			raise Exception('Not defined: n-1>= a >= b >= 0')
+	def __init__(self, a=[],k=0,n=0,scalar=1):
+		# Initializes cycle and checks if initial paramaters define a valid cycle
+		self.n = n
+		self.k = k
+		self.zpos = len(a)
+		for i in range(len(a)):
+			if a[i] == 0:
+				self.zpos = i
+				break
 		
-	def set_expr(self):
-		if self.b != 0:
-			return f"{{ L ϵ G(2,{self.n + 1}) | L ∩ P(V_({self.n - self.a}) ≠ Ø, L ⊆ P(V_({self.n + 1 - self.b}) }}"
+		ordered = True
+		if len(a) == 0:
+			self.a = [0 for i in range(k)]
+			self.scalar = scalar
+		elif a[0] <= n+1-k:
+			for i in range(len(a)-1):
+				if a[i] < a[i+1] or a[i+1] < 0:
+					ordered = False
+					break
+			if ordered:
+				self.a = a + [0 for i in range(k-len(a))]
+				self.scalar = scalar
+			else:
+				print(f"Not ordered {str(a)}")
+				self.a = [0 for i in range(k)]
+				self.scalar = 0
 		else:
-			return f"{{ L ϵ G(2,{self.n + 1}) | L ∩ P(V_({self.n - self.a}) ≠ Ø }}"
-		
+			self.a = [0 for i in range(k)]
+			self.scalar = 0
+
+	# Produces stringified version of a cycle for textual output
 	def __str__(self):
 		scalar_str = ""
-		if self.scalar != 1 or (self.a == 0 and self.b == 0):
+		if self.grading() == 0:
+			scalar_str = str(self.scalar)
+			return scalar_str
+		
+		if self.scalar == -1:
+			scalar_str = '-'
+		elif self.scalar != 1:
 			scalar_str = str(self.scalar)
 			
-		if self.b != 0:
-			return f"{scalar_str}σ_({self.a},{self.b})" 
-		elif self.a != 0:
-			return f"{scalar_str}σ_{self.a}"
-		else:
-			return f"{scalar_str}"
+		index_str = (str(self.a[:self.zpos])[1:-1]).replace(' ', '')
 		
+		return f"{scalar_str}σ_({index_str})" 
+		
+	# Checks if cycles belong to the same cohomology class, and not equality since scalars are disregarded
 	def __eq__(self, other):
-		return (self.a == other.a and self.b == other.b and self.n == other.n)
+		if self.k == other.k and self.n == other.n:
+			for i in range(self.k):
+				if self.a[i] != other.a[i]:
+					return False
+		else:
+			return False
+		return True
 		
+	# Returns the grading of the cycle in the cohomology ring
 	def grading(self):
-		return 2*(self.a+self.b)
+		return 2*(sum(self.a))
+
+	# Produces a scaled cycle using an integer scalar
+	def scale(self, alpha):
+		c = copy(self)
+		c.scalar *= alpha
+		return c
 		
+	# Uses the CSum class to compute sums of cycles
 	def __add__(self, other):
 		if type(other) is SchubertCycle:
 			cs = CSum()
@@ -100,73 +206,155 @@ class SchubertCycle:
 			other.add_cycle(self)
 			return other
 			
+	# Uses the CSum class to compute difference of cycles
+	def __sub__(self, other):
+		c = other.scale(-1)
+		return self+c
+
+	# Bootstraps multiplication to perform exponention, not well-optimized
 	def __pow__(self, k):
 		if k == 0:
-			return SchubertCycle(a=0,b=0,n=self.n)
-		elif k%2 == 1:
-			return self*(self**(k-1))
-		else:
-			return ((self**(int(k/2)))*(self**(int(k/2))))
+			return SchubertCycle(a=[], k=self.k,n=self.n)
+		t = self
+		for i in range(k-1):
+			t = t*self
+		return t
 
-			
-	def multiply(self, a,b,c, scalar):
+	# Negates the Schubert cycle, equivalent to scaling by -1
+	def __neg__(self):
+		return self.scale(-1)
+
+	# Computes the matrix to be used in the Giambelli's formula
+	def giambellis(self):
+		A = [[SchubertCycle(a=[self.a[i] + j-i ],k=self.k, n=self.n) for j in range(self.k)] for i in range(self.k)]
+		return A
+
+	# Multiplies two cycles a*b using Pieri's formula
+	def multiply(self, a, mu, scalar):
 		cs = CSum()
-		for d in range(b,self.n):
-			for e in range(c,b+1):
-				if d+e == a+b+c:
-					cycle = SchubertCycle(a=d, b=e, n=self.n)
-					cycle.scalar = scalar
-					cs.add(cycle)
+		possibilities = []
+		# Obtains all of the k-tuples that satisfy the range requirements given in Pieri's formula
+		for i in range(len(a)):
+			cis = []
+			lb = a[i]
+			ub = self.n
+			if i > 0:
+				ub = a[i-1] + 1
+			for ci in range(lb, ub):
+				cis.append([ci])
+			if len(possibilities) == 0:
+				possibilities = cis
+			else:
+				np = []
+				for p in possibilities:
+					for ci in cis:
+						np.append(p + ci)
+				possibilities = np
+
+		# Filters the k-tuples from above using the second condition of Pieri's formula, and produces the output
+		for p in possibilities:
+			if sum(p) == sum(a) + mu:
+				cs.add_cycle(SchubertCycle(a=p, k=self.k, n=self.n, scalar=scalar))
 		return cs
 		
+	# Checks if the cycle is primitive, i.e., is a tuple of the form (a1,0,...,0)
+	def is_primitive(self):
+		return self.zpos == 1
+
+	# Returns the string representation for the cycle for console output
 	def __repr__(self):
 		return str(self)
 	
+	# General case manager for cycle multiplication a*b
 	def __mul__(self, other):
+		# Distributes product across sum
 		if type(other) is CSum:
 			return other*self
-		if self.b != 0:
-			if other.b == 0:
-				return self.multiply(other.a, self.a, self.b, self.scalar * other.scalar)
-		if self.b == 0:
-			return self.multiply(self.a, other.a, other.b, self.scalar*other.scalar)
-			
-		if self.grading() + other.grading() > 4*(self.n-1):
+		# Checks if grading of product is greater than the dimension of the cohomology ring
+		if self.grading() + other.grading() > 2*(self.k * (self.n + 1 - self.k)):
 			return CSum()
-		elif other.a == self.n - 1 - self.b and other.b == self.n - 1 - self.a:
-			cs = CSum()
-			cs.add_cycle(SchubertCycle(a=self.n-1,b=self.n-1, n=self.n))
-			return cs
+		# Checks if cycle is in the 0-th cohomology class
+		elif self.grading() == 0:
+			other.scalar *= self.scalar
+			return copy(other)
+		elif other.grading() == 0:
+			self.scalar *= other.scalar
+			return copy(self)
+		# Checks if cycle is primitive, and if so, performs Pieri's formula
+		elif self.is_primitive():
+			return self.multiply(other.a, self.a[0], other.scalar * self.scalar)
+		elif other.is_primitive():
+			return self.multiply(self.a, other.a[0], other.scalar * self.scalar)
+		# Application of giambell's formula in cases when Pieri's formula cannot be applied directly
 		else:
-			return CSum()
-		
+			A = self.giambellis()
+			g = gdeterminant(copy(A))
+			cs = CSum()
+			for entry in g:
+				temp = copy(other)
+				for cycle in entry:
+					temp = temp*cycle
+				cs.add(temp)
+			return cs.scale(self.scalar)
+	
+	# Performs scalar multiplication of a cycle by an integer
+	def __rmul__(self, alpha):
+		c= copy(self)
+		if type(alpha) is int:
+			c.scalar *= alpha
+		else:
+			raise NotImplementedError
+		return c
+
+# Computes partitions of n-1 as used in finding the genrators for the cohomology classes
+def find_partitions(k,n):
+	if k == 1:
+		return [[i] for i in range(n)]
+	else:
+		a1s = [i for i in range(n)]
+		parts = []
+		for a in a1s:
+			a2s = find_partitions(k-1, a+1)
+			a_parts = [[a] + a2sp for a2sp in a2s]
+			parts += a_parts
+		return parts
 			
-def compute_cohomology_groups(n):
-	groups = [[] for i in range(4*(n-1) + 1)]
-	for b in range(n):
-		for a in range(b,n):
-			groups[2*(a+b)].append(SchubertCycle(a=a,b=b,n=n))
+# Computes the cohomology generators for the Grassmannian G(k,n+1)
+def compute_cohomology_groups(k,n):
+	groups = [[] for i in range(2*k*(n+1-k) + 1)]
+	partitions = find_partitions(k,n-k+2)
+	for a in partitions:
+		cycle = SchubertCycle(a=a, k=k, n=n)
+		idx = cycle.grading()
+		# print(idx)
+		groups[idx].append(cycle)
 	return groups
 			
-def print_cohomology_groups(groups):
-	td = len(groups)
-	n = int((td-1)/4+1)
+# Prints the cohomology groups with genrators
+def print_cohomology_groups(k,n):
+	groups = compute_cohomology_groups(k,n)
+	n = groups[0][0].n
+	k = groups[0][0].k
 	for i in range(len(groups)):
 		str_gens = [str(cycle) for cycle in groups[i]]
 		zstr_gens = [f"ℤ{cycle}" for cycle in str_gens]
 		if len(str_gens) == 0:
-			print(f"H^{i} (G(2,{n+1})) = 0")
+			print(f"H^{i} (G({k},{n+1})) = 0")
 		else:
-			print(f"H^{i} (G(2,{n+1})) = {'⊕'.join(zstr_gens)}")
+			print(f"H^{i} (G({k},{n+1})) = {'⊕'.join(zstr_gens)}")
 			
-def print_multiplication_table(groups):
+# Prints the multiplication table of a corresponding ring or subring of the Grassmannian, allows for LaTeX output as well as console print
+def print_multiplication_table(groups, latex=False):
 	gens = []
 	output = []
 	max_length = 1
 	for d in groups:
 		for c in d:
 			gens.append(c)
-	strgens = ['*']
+	if latex:
+		strgens = ["\\cdot"]
+	else:
+		strgens = ['*']
 	for g in gens:
 		sg = str(g)
 		l = len(str(g))
@@ -178,27 +366,37 @@ def print_multiplication_table(groups):
 	for c1 in gens:
 		o = [str(c1)]
 		for c2 in gens:
-			c3 = c1*c2
 			c3s = str(c1*c2)
 			l = len(c3s)
 			if max_length < l:
 				max_length = l
 			o.append(c3s)
 		output.append(o)
-	
-	spacing = lambda n : ''.join([' ' for i in range(n)])
-	padding = spacing(int(max_length/2))
-	headers = True
-	for o in output:
-		line = "|"
-		for e in o:
-			diff = max_length - len(e)
-			tstr = padding + e + spacing(diff) + padding + '|'
-			line += tstr
-		print(line)
-		if headers:
-			headers = False
-			print(''.join(['-' for i in range(len(line))]))
-	
-		
-
+	if not latex:
+		spacing = lambda n : ''.join([' ' for i in range(n)])
+		padding = spacing(int(max_length/2))
+		headers = True
+		for o in output:
+			line = "|"
+			for e in o:
+				diff = max_length - len(e)
+				tstr = padding + e + spacing(diff) + padding + '|'
+				line += tstr
+			print(line)
+			if headers:
+				headers = False
+				print(''.join(['-' for i in range(len(line))]))
+	else:
+		cols = ' | '.join(['c' for i in strgens])
+		print (f'\\begin{{tabular}}{{ | {cols} | }}')
+		print('\\hline')
+		headers = True
+		for o in output:
+			line = ""
+			for e in o:
+				tstr = f" ${e}$ & "
+				line += tstr
+			tline = line[:-2].replace('σ', '\\sigma').replace('(', '{').replace(')', '}').replace('+-','-') + '\\\\'
+			print(tline)
+			print('\\hline')
+		print ('\\end{tabular}')
